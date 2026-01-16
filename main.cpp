661d903e5568534ac8bec077bb595f04bf92dc35
therealMaPMaKeR <stonedmapster@gmail.com>
2026-01-16 02:29:40 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QFileDialog>
+#include <QDebug>
 #include "lightweightvideoplayer.h"
 
 int main(int argc, char *argv[])
@@ -10,11 +11,20 @@ int main(int argc, char *argv[])
     LightweightVideoPlayer player;
     player.show();
     
-    // Optionally, open a file dialog to select a video
-    QString fileName = QFileDialog::getOpenFileName(&player,
-        QObject::tr("Open Video File"),
-        QString(),
-        QObject::tr("Video Files (*.mp4 *.avi *.mkv *.mov *.wmv *.flv *.webm);;All Files (*.*)"));
+    QString fileName;
+    
+    // Check if a file was passed as a command-line argument
+    if (argc > 1) {
+        // File path was provided (e.g., from double-clicking a video file)
+        fileName = QString::fromLocal8Bit(argv[1]);
+        qDebug() << "Opening file from command line:" << fileName;
+    } else {
+        // No file provided, show file dialog
+        fileName = QFileDialog::getOpenFileName(&player,
+            QObject::tr("Open Video File"),
+            QString(),
+            QObject::tr("Video Files (*.mp4 *.avi *.mkv *.mov *.wmv *.flv *.webm);;All Files (*.*)"));
+    }
     
     if (!fileName.isEmpty()) {
         player.loadVideo(fileName);
